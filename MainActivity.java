96e81955e620ab717e18a397c27967a57099d638
Ahsan Saeed <ahsan02@gmail.com>
2021-07-04 22:41:22 -0400

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -3,8 +3,11 @@ package com.example.rpgsurvivalgame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,10 +15,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Typeface type = Typeface.createFromAsset(getAssets(),"fonts/Enchanted_Land.otf");
+        TextView textView = (TextView)findViewById(R.id.textView);
+        Button button = (Button)findViewById(R.id.button);
+        textView.setTypeface(type);
+        button.setTypeface(type);
     }
 
 
     public void startGame(View view) {
+        /*
+        Called when user presses the 'PLAY GAME' button. Calls the GameActivity Activity.
+         */
         Intent intent = new Intent(this, GameActivity.class);
         startActivity(intent);
     }
